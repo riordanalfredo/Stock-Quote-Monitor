a7e28d53e26777ca07ce8c2bd07ee2c9fea352c5
Kelvin Kwok <kjkwo2@student.monash.edu>
2018-04-24 02:40:20 +1000

    Creating MVC files and structure
    Created Stockquote for MVC, Model, View and Controller. Have not implemented Observer class yet

---
+++ src/Model/StockQuoteModel.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package Model;
+
+/**
+ *  @author: kjkwo2@student.monash.edu
+ *  Created 24 April 2018
+ */
+//TODO Associate with Observer abstract class -----------------------------
+//Model class for Stock Quote.
+//Contains Storage of the Data from the Proxy server
+public class StockQuoteModel {
+	//Create an arraylist for the data that is stored (may need to change to static)
+	private ArrayList<String> Storage = new ArrayList<String>();
+	
+	
+	//Method that adds the symbol into the interface
+	public void addStock(String symbol){
+		
+		
+		
+		}
+
+	//Method that removes the symbol from the interface
+	public void removeStock(String symbol){
+			
+			
+			
+		}
+
+	//Method for the View class to retrieve the storage of the symbols/data.
+	public String getStockStorage(){
+		
+		return Storage;
+		
+		}
+	
+}
